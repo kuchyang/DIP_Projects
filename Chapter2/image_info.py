30f9e3b7d08f017dedb5305cd156453d555e7f29
楊逸平 <75960978+kuchyang@users.noreply.github.com>
2021-03-16 17:40:15 +0800

    顯示影像資訊

--- Chapter2/image_info.py
+++ Chapter2/image_info.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 
 
-filename = input( "Please enter filename: " )  #請輸入文件名
+filename = input( "Enter filename: " )  #輸入文件名
 img = cv2.imread( filename, -1 ) 
 
 nr, nc = img.shape[:2] 
@@ -9,6 +9,6 @@ print( "Number of Rows =", nr )  #行數
 print( "Number of Columns =", nc)  #列數
 
 if img.ndim != 3:
-  print( "Gray-Level Image") 
+  print("Gray-Level Image") 
 else: 
-  print( "Color Image")
+  print("Color Image")
