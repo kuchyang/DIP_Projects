8df357234ebf40e13add1aac974939602037d3c3
楊逸平 <75960978+kuchyang@users.noreply.github.com>
2021-03-17 20:39:15 +0800

    Update Grayscale_gradual-image.py

--- Chapter2/Grayscale_gradual-image.py
+++ Chapter2/Grayscale_gradual-image.py
@@ -1,2 +1,8 @@
 import numpy as np
 import cv2
+
+row = np.array( [(i/2, i/2, i/2) for i in range(512)], dtype="uint8" )
+img = np.repeat( np.array([row], dtype="uint8"), 100, axis=0 )
+cv2.imwrite( 'color_img.jpg', img )  #將NumPy陣列中儲存的圖片寫入檔案
+cv2.imshow( "image",img )
+cv2.waitKey(0)
