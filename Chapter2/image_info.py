01c3ccab4bbcd33e1ab03b6348ae3954ae595029
楊逸平 <75960978+kuchyang@users.noreply.github.com>
2021-03-17 20:27:30 +0800

    顯示影像資訊

--- Chapter2/image_info.py
+++ Chapter2/image_info.py
@@ -2,9 +2,9 @@ import numpy as np
 import cv2 
 
 filename = input("Enter filename: ")  #輸入文件名
-img = cv2.imread( filename, -1 ) 
+img = cv2.imread( filename, -1 )   #讀取圖像
 
-nr, nc = img.shape[:2] 
+nr, nc = img.shape[:2]  #shape屬性獲取圖像大小.(第一元素表示圖像的高度,第二表示圖像的寬度,第三表示像素的通道數)
 print( "Number of Rows =", nr )  #行數
 print( "Number of Columns =", nc)  #列數
 
