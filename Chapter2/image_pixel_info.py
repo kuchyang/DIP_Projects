cc9ed82d734dfdc800b086ebe1d80e2d588f7c65
楊逸平 <75960978+kuchyang@users.noreply.github.com>
2021-03-17 20:20:25 +0800

    顯示像素資訊

--- Chapter2/image_pixel_info.py
+++ Chapter2/image_pixel_info.py
@@ -1,20 +1,20 @@
 import numpy as np
 import cv2
 
-global img
+global img  #全域變數
 
 def onMouse( event, x, y, flags, param ):
-    x,y = y,x
+    x,y = y,x  #cv2的[軸]定義與數位影像相反,因此要交換
     if img.ndim != 3:
         print( "(x,y) = (%d,%d)" %(x,y), end = "  " )
         print( "Gray-Level = %3d" %img[x,y] )
     else:
         print( "(x,y) = (%d,%d)" %(x,y), end = "  " )
-        print( "(R,G,B) = (%3d,%3d,%3d)" %(img[x,y,2],img[x,y,1],img[x,y,0]) )
+        print( "(R,G,B) = (%3d,%3d,%3d)" %(img[x,y,2],img[x,y,1],img[x,y,0]) )  #cv2存取順序式(B,G,R)
 
 filename = input( "Please enter filename:" )
 img = cv2.imread( filename,-1 )
 cv2.namedWindow( filename )
-cv2.setMouseCallback( filename, onMouse )
+cv2.setMouseCallback( filename, onMouse )  #回呼(Callback)函式:滑鼠觸發事件(event),呼叫副函式(onMouse)
 cv2.imshow( filename,img )
 cv2.waitKey(0)
