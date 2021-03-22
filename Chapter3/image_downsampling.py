db28b5071db7e934c743cd5747756a0d1867974c
楊逸平 <75960978+kuchyang@users.noreply.github.com>
2021-03-22 20:18:52 +0800

    下取樣

---
+++ Chapter3/image_downsampling.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+def image_downsampling( f, sampling_rate ):
+    nr,nc = f.shape[:2]  #獲取圖像大小
+    nr_s,nc_s = nr//sampling_rate, nc//sampling_rate
+    g = np.zeros( [nr_s,nc_s], dtype='uint8' )
+    for x in range(nr_s):
+        for y in range(nc_s):
+            g[x,y] = f[x*sampling_rate, y*sampling_rate]
+    return g
+
+def main():
+    filename = input("Enter filename: ")  #輸入文件名
+    img1 = cv2.imread( filename, -1 )   #讀取圖像
+    img2 = image_downsampling( img1, 2 )
+    cv2.imshow( "original", img1 )  #顯示original圖像
+    cv2.imshow( "downsampling", img2 )  #顯示image formation model圖像
+    cv2.waitKey(0)
+    
+main()
