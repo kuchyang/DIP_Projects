adf31dd111e7c6a489fe3f0bb0fdbf687b0279be
楊逸平 <75960978+kuchyang@users.noreply.github.com>
2021-04-10 15:42:46 +0800

    Create image_rotation.py

---
+++ Chapter4/image_rotation.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+filename = input("Enter filename: ")  #輸入文件名
+img1 = cv2.imread(filename, -1 )   #讀取圖像
+nr2,nc2 = img1.shape[:2]
+
+rotation_matrix = cv2.getRotationMatrix2D( (nr2/2, nc2/2), 30, 1 )
+rotation_matrix_1 = cv2.getRotationMatrix2D( (0, 0), 30, 1 )
+img2 = cv2.warpAffine( img1, rotation_matrix, (nr2, nc2) )
+img3 = cv2.warpAffine( img1, rotation_matrix_1, (nr2, nc2) )
+
+cv2.imshow( "original", img1 )  #顯示original圖像
+cv2.imshow( "rotation_matrix", img2 )  #顯示圖像
+cv2.imshow( "rotation_matrix_1", img3 )
+cv2.waitKey(0)
+cv2.destroyAllWindows()
