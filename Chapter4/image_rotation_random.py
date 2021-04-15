6adccc02d6156aee901e25ea5d6e87d24569603e
楊逸平 <75960978+kuchyang@users.noreply.github.com>
2021-04-15 20:40:04 +0800

    Update image_rotation_random.py

--- Chapter4/image_rotation_random.py
+++ Chapter4/image_rotation_random.py
@@ -4,24 +4,18 @@ import cv2
 def image_scale( Degree, nr,nc):
     Rad = Degree * np.pi / 180
     scale = np.sin(Rad) 
-    scale45=np.sin(45 * np.pi / 180)
-    print(scale)
+    #scale45=np.sin(45 * np.pi / 180)
+    #print(scale)
     Deg1=Degree%90
     if Deg1 == 0:
         size = 1
     elif Deg1 > 45:
-        p=pow(np.sin(Rad),2)+pow(np.cos(Rad),2)
-        np.sqrt(p)
-        size =  scale45+(1-scale)*(Deg1-45)/45
-        
+        #p=pow(np.sin(Rad),2)+pow(np.cos(Rad),2)
+        #np.sqrt(p)
+        size = np.sin(45 * np.pi / 180)
     elif Deg1 < 45:
-        p=pow(np.sin(Rad),4) + pow(np.cos(Rad),4)
-        print(np.sin(Rad))
-        print(np.cos(Rad))
-        np.sqrt(p)
-        size = np.sqrt(p)#1-(1-scale45)*(Deg1)/45
-        print(size)
-        
+        size = np.cos(45 * np.pi / 180) #1-(1-scale45)*(Deg1)/45
+        #print(size)
     else :
         size = scale
     return size
